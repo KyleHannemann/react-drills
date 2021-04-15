9fc50fae671d05dc282fef2591d6337306d18977
Kyle Hannemann <hannemannkyle@gmail.com>
2021-04-15 10:52:13 -0600

    finished app7

--- app-7/src/List.js
+++ app-7/src/List.js
@@ -4,7 +4,7 @@ export default class List extends Component{
     constructor(props){
         super(props);
 
-    }
+}
     render(){
         let remove = this.props.remove
         let list = this.props.list.map(function(el, index){
